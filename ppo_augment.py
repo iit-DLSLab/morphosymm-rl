d7ec9afada77ae4db069045f97f6be8f6b388c43
Daniel Ordonez <daniels.ordonez@gmail.com>
2025-03-14 23:57:45 -0500

    Generic obs/act space group representations

--- ppo_augment.py
+++ ppo_augment.py
@@ -8,8 +8,13 @@ import warnings
 from rsl_rl.modules import ActorCritic
 from rsl_rl.storage import RolloutStorage
 
-#from morpho_symm.nn.test_EMLP import get_kinematic_three_rep_two, get_ground_reaction_forces_rep_two, get_friction_rep
-from test_EMLP import get_kinematic_three_rep_two, get_ground_reaction_forces_rep_two, get_friction_rep
+from symm_utils import configure_observation_space_representations
+# from morpho_symm.nn.test_EMLP import get_kinematic_three_rep_two, get_ground_reaction_forces_rep_two, get_friction_rep
+from test_EMLP import (
+    get_kinematic_three_rep_two,
+    get_ground_reaction_forces_rep_two,
+    get_friction_rep,
+)
 
 
 import escnn
@@ -20,31 +25,33 @@ from morpho_symm.utils.robot_utils import load_symmetric_system
 
 from hydra.core.global_hydra import GlobalHydra
 
+
 class PPOAugmented:
     """Proximal Policy Optimization algorithm with data augmentation via symmetries."""
 
     actor_critic: ActorCritic
     """The actor critic module."""
 
-    def __init__(self,
-                 actor_critic,
-                 #task, #to remove
-                 num_learning_epochs=1,
-                 num_mini_batches=1,
-                 clip_param=0.2,
-                 gamma=0.998,
-                 lam=0.95,
-                 value_loss_coef=1.0,
-                 entropy_coef=0.0,
-                 learning_rate=1e-3,
-                 max_grad_norm=1.0,
-                 use_clipped_value_loss=True,
-                 schedule="fixed",
-                 desired_kl=0.01,
-                 device='cpu',
-                 normalize_advantage_per_mini_batch=False, #added
-                 ):
-
+    def __init__(
+        self,
+        actor_critic,
+        # task, #to remove
+        num_learning_epochs=1,
+        num_mini_batches=1,
+        clip_param=0.2,
+        gamma=0.998,
+        lam=0.95,
+        value_loss_coef=1.0,
+        entropy_coef=0.0,
+        learning_rate=1e-3,
+        max_grad_norm=1.0,
+        use_clipped_value_loss=True,
+        schedule="fixed",
+        desired_kl=0.01,
+        device="cpu",
+        normalize_advantage_per_mini_batch=False,  # added
+        robot_name: str = "a1",
+    ):
         self.device = device
 
         self.desired_kl = desired_kl
@@ -78,58 +85,40 @@ class PPOAugmented:
         self.symmetry = None
 
         # MorphoSymm components - our addition goes here!!
-        global G
-        GlobalHydra.instance().clear()
-        initialize(config_path="./MorphoSymm/morpho_symm/cfg/robot", version_base='1.3')
-        robot_name = 'a1'  # or any of the robots in the library (see `/morpho_symm/cfg/robot`)
-        robot_cfg = compose(config_name=f"{robot_name}.yaml")
-        robot, G = load_symmetric_system(robot_cfg=robot_cfg)
+        obs_space_names = [
+            "base_lin_vel:base",
+            "base_ang_vel:base",
+            "gravity:base",
+            "ctrl_commands",
+            "default_qpos_js_error",
+            "qvel_js",
+            "actions",
+            "clock_data",
+        ]
+        action_space_names = ["actions"]
 
-        # We use ESCNN to handle the group/representation-theoretic concepts and for the construction of equivariant neural networks.
+        G, obs_reps = configure_observation_space_representations(robot_name, obs_space_names)
+
+        obs_space_reps = [obs_reps[n] for n in obs_space_names] * 3
+        act_space_reps = [obs_reps[n] for n in action_space_names]
+        # rep_extra_obs = [rep_R3, rep_R3_pseudo, trivial_rep, trivial_rep, rep_friction, rep_R3, trivial_rep, trivial_rep, rep_kin_three, rep_kin_three, rep_kin_three, rep_kin_three, trivial_rep, trivial_rep]
+
         gspace = escnn.gspaces.no_base_space(G)
-        # Get the relevant group representations.
-        rep_QJ = G.representations["Q_js"]  # Used to transform joint-space position coordinates q_js ∈ Q_js
-        rep_TqQJ = G.representations["TqQ_js"]  # Used to transform joint-space velocity coordinates v_js ∈ TqQ_js
-        # IT WAS RD and RD_pseudo
-        rep_O3 = G.representations["R3"]  # Used to transform the linear momentum l ∈ R3
-        rep_O3_pseudo = G.representations["R3_pseudo"]  # Used to transform the angular momentum k ∈ R3
-        trivial_rep = G.trivial_representation
-        rep_kin_three = get_kinematic_three_rep_two(G)
-        rep_friction = get_friction_rep(G, rep_kin_three)
-        # Define the input and output FieldTypes using the representations of each geometric object.
-        # Representation of x := [q, v] ∈ Q_js x TqQ_js      =>    ρ_X_js(g) := ρ_Q_js(g) ⊕ ρ_TqQ_js(g)  | g ∈ G
-        """obs = torch.cat(
-            [
-                tensor
-                for tensor in (
-                    self._robot.data.root_lin_vel_b,
-                    self._robot.data.root_ang_vel_b,
-                    self._robot.data.projected_gravity_b,
-                    obs_commands,
-                    self._robot.data.joint_pos - self._robot.data.default_joint_pos,
-                    self._robot.data.joint_vel,
-                    self._actions,
-                    clock_data,
-                )
-                if tensor is not None
-            ],
-            dim=-1,
-        )"""
-        base_transition = [rep_O3, rep_O3_pseudo, rep_O3, rep_O3_pseudo, rep_QJ, rep_TqQJ, rep_QJ, rep_kin_three, rep_kin_three] * 3
-        rep_extra_obs = [rep_O3, rep_O3_pseudo, trivial_rep, trivial_rep, rep_friction, rep_O3, trivial_rep, trivial_rep, rep_kin_three, rep_kin_three, rep_kin_three, rep_kin_three, trivial_rep, trivial_rep]
+        self.in_field_type = FieldType(gspace, obs_space_reps)
+        self.out_field_type = FieldType(gspace, act_space_reps)
 
-
-        self.in_field_type = FieldType(gspace, base_transition)
-        # Representation of y := [l, k] ∈ R3 x R3            =>    ρ_Y_js(g) := ρ_O3(g) ⊕ ρ_O3pseudo(g)  | g ∈ G
-        self.out_field_type = FieldType(gspace, [rep_QJ])
-
-        #self.critic_in_field_type = FieldType(gspace, base_transition + rep_extra_obs)
-        self.critic_in_field_type = FieldType(gspace, base_transition)
-
+        self.critic_in_field_type = FieldType(gspace, obs_space_reps)
         self.num_replica = len(G.elements)
         self.G = G
 
-    def init_storage(self, num_envs, num_transitions_per_env, actor_obs_shape, critic_obs_shape, action_shape):
+    def init_storage(
+        self,
+        num_envs,
+        num_transitions_per_env,
+        actor_obs_shape,
+        critic_obs_shape,
+        action_shape,
+    ):
         # create memory for RND as well :)
         if self.rnd:
             rnd_state_shape = [self.rnd.num_states]
@@ -137,7 +126,7 @@ class PPOAugmented:
             rnd_state_shape = None
         # create rollout storage
         self.storage = RolloutStorage(
-            num_envs * self.num_replica, #added here,
+            num_envs * self.num_replica,  # added here,
             num_transitions_per_env,
             actor_obs_shape,
             critic_obs_shape,
@@ -148,7 +137,7 @@ class PPOAugmented:
 
     def test_mode(self):
         self.actor_critic.test()
-    
+
     def train_mode(self):
         self.actor_critic.train()
 
@@ -158,14 +147,16 @@ class PPOAugmented:
         # Compute the actions and values
         self.transition.actions = self.actor_critic.act(obs).detach()
         self.transition.values = self.actor_critic.evaluate(critic_obs).detach()
-        self.transition.actions_log_prob = self.actor_critic.get_actions_log_prob(self.transition.actions).detach()
+        self.transition.actions_log_prob = self.actor_critic.get_actions_log_prob(
+            self.transition.actions
+        ).detach()
         self.transition.action_mean = self.actor_critic.action_mean.detach()
         self.transition.action_sigma = self.actor_critic.action_std.detach()
         # need to record obs and critic_obs before env.step()
         self.transition.observations = obs
         self.transition.critic_observations = critic_obs
         return self.transition.actions
-    
+
     def process_env_step(self, rewards, dones, infos):
         # Record the rewards and dones
         # Note: we clone here because later on we bootstrap the rewards based on timeouts
@@ -173,8 +164,12 @@ class PPOAugmented:
         self.transition.dones = dones
 
         # Bootstrapping on time outs
-        if 'time_outs' in infos:
-            self.transition.rewards += self.gamma * torch.squeeze(self.transition.values * infos['time_outs'].unsqueeze(1).to(self.device), 1)
+        if "time_outs" in infos:
+            self.transition.rewards += self.gamma * torch.squeeze(
+                self.transition.values
+                * infos["time_outs"].unsqueeze(1).to(self.device),
+                1,
+            )
 
         # Augment the transition
         self.augment_transitions()
@@ -190,29 +185,63 @@ class PPOAugmented:
         in_field_type = self.in_field_type
         critic_in_field_type = self.critic_in_field_type
         G = self.G
-        t.actions = torch.cat([t.actions] + [out_field_type.transform_fibers(t.actions, g) for g in G.elements[1:]], dim=0)
+        t.actions = torch.cat(
+            [t.actions]
+            + [out_field_type.transform_fibers(t.actions, g) for g in G.elements[1:]],
+            dim=0,
+        )
         t.actions_log_prob = torch.cat([t.actions_log_prob] * self.num_replica, dim=0)
-        t.action_mean = torch.cat([t.action_mean] + [out_field_type.transform_fibers(t.action_mean, g) for g in G.elements[1:]], dim=0)
-        t.action_sigma = torch.abs(torch.cat([t.action_sigma] + [out_field_type.transform_fibers(t.action_sigma, g) for g in G.elements[1:]], dim=0))
+        t.action_mean = torch.cat(
+            [t.action_mean]
+            + [
+                out_field_type.transform_fibers(t.action_mean, g)
+                for g in G.elements[1:]
+            ],
+            dim=0,
+        )
+        t.action_sigma = torch.abs(
+            torch.cat(
+                [t.action_sigma]
+                + [
+                    out_field_type.transform_fibers(t.action_sigma, g)
+                    for g in G.elements[1:]
+                ],
+                dim=0,
+            )
+        )
         t.values = torch.cat([t.values] * self.num_replica, dim=0)
         t.rewards = torch.cat([t.rewards] * self.num_replica, dim=0)
         t.dones = torch.cat([t.dones] * self.num_replica, dim=0)
-        t.observations = torch.cat([t.observations] + [in_field_type.transform_fibers(t.observations, g) for g in G.elements[1:]], dim=0)
-        t.critic_observations = torch.cat([t.critic_observations] + [critic_in_field_type.transform_fibers(t.critic_observations, g) for g in G.elements[1:]], dim=0)
-
+        t.observations = torch.cat(
+            [t.observations]
+            + [
+                in_field_type.transform_fibers(t.observations, g)
+                for g in G.elements[1:]
+            ],
+            dim=0,
+        )
+        t.critic_observations = torch.cat(
+            [t.critic_observations]
+            + [
+                critic_in_field_type.transform_fibers(t.critic_observations, g)
+                for g in G.elements[1:]
+            ],
+            dim=0,
+        )
 
     def augment_values(self, values):
         values = torch.cat([values] * self.num_replica, dim=0)
         return values
 
-
     def compute_returns(self, last_critic_obs):
-        last_values= self.actor_critic.evaluate(last_critic_obs).detach()
+        last_values = self.actor_critic.evaluate(last_critic_obs).detach()
         last_values = self.augment_values(last_values)
         self.storage.compute_returns(
-            last_values, self.gamma, self.lam, normalize_advantage=not self.normalize_advantage_per_mini_batch
+            last_values,
+            self.gamma,
+            self.lam,
+            normalize_advantage=not self.normalize_advantage_per_mini_batch,
         )
-
 
     def update(self):
         mean_value_loss = 0
@@ -223,9 +252,13 @@ class PPOAugmented:
 
         # generator for mini batches
         if self.actor_critic.is_recurrent:
-            generator = self.storage.recurrent_mini_batch_generator(self.num_mini_batches, self.num_learning_epochs)
+            generator = self.storage.recurrent_mini_batch_generator(
+                self.num_mini_batches, self.num_learning_epochs
+            )
         else:
-            generator = self.storage.mini_batch_generator(self.num_mini_batches, self.num_learning_epochs)
+            generator = self.storage.mini_batch_generator(
+                self.num_mini_batches, self.num_learning_epochs
+            )
 
         # iterate over batches
         for (
@@ -242,7 +275,6 @@ class PPOAugmented:
             masks_batch,
             rnd_state_batch,
         ) in generator:
-
             # number of augmentations per sample
             # we start with 1 and increase it if we use symmetry augmentation
             num_aug = 1
@@ -252,15 +284,19 @@ class PPOAugmented:
             # check if we should normalize advantages per mini batch
             if self.normalize_advantage_per_mini_batch:
                 with torch.no_grad():
-                    advantages_batch = (advantages_batch - advantages_batch.mean()) / (advantages_batch.std() + 1e-8)
-
-
+                    advantages_batch = (advantages_batch - advantages_batch.mean()) / (
+                        advantages_batch.std() + 1e-8
+                    )
 
             # Recompute actions log prob and entropy for current batch of transitions
             # Note: we need to do this because we updated the actor_critic with the new parameters
             # -- actor
-            self.actor_critic.act(obs_batch, masks=masks_batch, hidden_states=hid_states_batch[0])
-            actions_log_prob_batch = self.actor_critic.get_actions_log_prob(actions_batch)
+            self.actor_critic.act(
+                obs_batch, masks=masks_batch, hidden_states=hid_states_batch[0]
+            )
+            actions_log_prob_batch = self.actor_critic.get_actions_log_prob(
+                actions_batch
+            )
             # -- critic
             value_batch = self.actor_critic.evaluate(
                 critic_obs_batch, masks=masks_batch, hidden_states=hid_states_batch[1]
@@ -276,7 +312,10 @@ class PPOAugmented:
                 with torch.inference_mode():
                     kl = torch.sum(
                         torch.log(sigma_batch / old_sigma_batch + 1.0e-5)
-                        + (torch.square(old_sigma_batch) + torch.square(old_mu_batch - mu_batch))
+                        + (
+                            torch.square(old_sigma_batch)
+                            + torch.square(old_mu_batch - mu_batch)
+                        )
                         / (2.0 * torch.square(sigma_batch))
                         - 0.5,
                         axis=-1,
@@ -292,7 +331,9 @@ class PPOAugmented:
                         param_group["lr"] = self.learning_rate
 
             # Surrogate loss
-            ratio = torch.exp(actions_log_prob_batch - torch.squeeze(old_actions_log_prob_batch))
+            ratio = torch.exp(
+                actions_log_prob_batch - torch.squeeze(old_actions_log_prob_batch)
+            )
             surrogate = -torch.squeeze(advantages_batch) * ratio
             surrogate_clipped = -torch.squeeze(advantages_batch) * torch.clamp(
                 ratio, 1.0 - self.clip_param, 1.0 + self.clip_param
@@ -301,17 +342,20 @@ class PPOAugmented:
 
             # Value function loss
             if self.use_clipped_value_loss:
-                value_clipped = target_values_batch + (value_batch - target_values_batch).clamp(
-                    -self.clip_param, self.clip_param
-                )
+                value_clipped = target_values_batch + (
+                    value_batch - target_values_batch
+                ).clamp(-self.clip_param, self.clip_param)
                 value_losses = (value_batch - returns_batch).pow(2)
                 value_losses_clipped = (value_clipped - returns_batch).pow(2)
                 value_loss = torch.max(value_losses, value_losses_clipped).mean()
             else:
                 value_loss = (returns_batch - value_batch).pow(2).mean()
 
-            loss = surrogate_loss + self.value_loss_coef * value_loss - self.entropy_coef * entropy_batch.mean()
-
+            loss = (
+                surrogate_loss
+                + self.value_loss_coef * value_loss
+                - self.entropy_coef * entropy_batch.mean()
+            )
 
             # Random Network Distillation loss
             if self.rnd:
@@ -341,7 +385,6 @@ class PPOAugmented:
             # -- RND loss
             if mean_rnd_loss is not None:
                 mean_rnd_loss += rnd_loss.item()
-
 
         # -- For PPO
         num_updates = self.num_learning_epochs * self.num_mini_batches
---
+++ symm_utils.py
@@ -0,0 +1,89 @@
+# Created by Daniel Ordoñez (daniels.ordonez@gmail.com) at 14/03/25
+from typing import Sequence
+
+import numpy as np
+from escnn.group import Group, Representation
+
+
+def configure_observation_space_representations(
+        robot_name: str, obs_names: Sequence[str]
+        ) -> [Group, dict[str, Representation]]:
+    try:
+        import morpho_symm
+        from morpho_symm.utils.rep_theory_utils import (
+            escnn_representation_form_mapping,
+            group_rep_from_gens,
+            )
+        from morpho_symm.utils.robot_utils import load_symmetric_system
+    except ImportError as e:
+        raise ImportError('morpho_symm package is required to configure observation group representations') from e
+
+    G = load_symmetric_system(robot_name=robot_name, return_robot=False)
+    rep_Q_js = G.representations['Q_js']  # Representation on joint space position coordinates
+    rep_TqQ_js = G.representations['TqQ_js']  # Representation on joint space velocity coordinates
+    rep_Rd = G.representations['R3']  # Representation on vectors in R^d
+    rep_Rd_pseudo = G.representations['R3_pseudo']  # Representation on pseudo vectors in R^d
+    rep_euler_xyz = G.representations['euler_xyz']  # Representation on Euler angles
+    # TODO: Ensure the limb order in the configuration matches the used order by quadruped gym.
+    rep_kin_three = G.representations['kin_chain']  # Permutation of legs
+    rep_Rd_on_limbs = rep_kin_three.tensor(rep_Rd)  # Representation on signals R^d on the limbs
+    rep_Rd_on_limbs.name = 'Rd_on_limbs'
+    rep_Rd_pseudo_on_limbs = rep_kin_three.tensor(rep_Rd_pseudo)  # Representation on pseudo vect R^d on the limbs
+    rep_Rd_pseudo_on_limbs.name = 'Rd_pseudo_on_limbs'
+    rep_SO3_flat = {}
+    for h in G.elements:
+        rep_SO3_flat[h] = np.kron(rep_Rd(h), rep_Rd(~h).T)
+    rep_SO3_flat = escnn_representation_form_mapping(G, rep_SO3_flat)
+    rep_SO3_flat.name = 'SO3_flat'
+
+    # Create a representation for the z dimension alone of the base position
+    rep_z = escnn_representation_form_mapping(G, {g: rep_Rd(g)[2:3, 2:3] for g in G.elements}, name='base_z')
+    rep_roll = escnn_representation_form_mapping(
+        G, {g: rep_Rd_pseudo(g)[0:1, 0:1] for g in G.elements}, name='base_roll'
+        )
+    rep_pitch = escnn_representation_form_mapping(
+        G, {g: rep_Rd_pseudo(g)[1:2, 1:2] for g in G.elements}, name='base_pitch'
+        )
+    rep_yaw = escnn_representation_form_mapping(G, {g: rep_Rd_pseudo(g)[2:3, 2:3] for g in G.elements}, name='base_yaw')
+
+    obs_reps = {}
+    for obs_name in obs_names:
+        # Generalized position, velocity, and force (torque) spaces
+        if obs_name == 'qpos':
+            continue  # Quaternion does not have a left-group action definition.
+        elif obs_name == 'qvel':
+            obs_reps[obs_name] = rep_Rd + rep_Rd_pseudo + rep_TqQ_js  # lin_vel , ang_vel, joint_vel
+        elif obs_name in ['tau_ctrl_setpoint', 'actions']:
+            obs_reps[obs_name] = rep_TqQ_js
+        elif 'qpos_js' in obs_name:  # Joint space position configuration
+            obs_reps[obs_name] = rep_Q_js
+        elif 'qvel_js' in obs_name:  # Joint space velocity configuration
+            obs_reps[obs_name] = rep_TqQ_js
+        elif obs_name == 'base_pos':
+            obs_reps[obs_name] = rep_Rd
+        elif obs_name == 'base_pos_z':
+            obs_reps[obs_name] = rep_z
+        elif 'base_lin_vel' in obs_name or 'base_lin_acc' in obs_name:  # base_lin_vel / base_lin_vel:base (base frame)
+            obs_reps[obs_name] = rep_Rd
+        elif 'base_ang_vel' in obs_name:
+            obs_reps[obs_name] = rep_Rd_pseudo
+        elif 'base_ori_euler_xyz' in obs_name:
+            obs_reps[obs_name] = rep_euler_xyz
+        elif obs_name == 'base_ori_quat_wxyz':
+            continue  # Quaternion does not have a left-group action definition.
+        elif obs_name == 'base_ori_SO3':
+            obs_reps[obs_name] = rep_SO3_flat
+        elif 'feet_pos' in obs_name or 'feet_vel' in obs_name:  # feet_pos:frame := feet_pos:world or feet_pos:base
+            obs_reps[obs_name] = rep_Rd_on_limbs
+        elif obs_name in ['contact_state', 'clock_data']:
+            obs_reps[obs_name] = rep_kin_three
+        elif 'contact_forces' in obs_name:
+            obs_reps[obs_name] = rep_Rd_on_limbs
+        elif 'gravity' in obs_name or 'imu_acc' in obs_names:
+            obs_reps[obs_name] = rep_Rd
+        elif 'imu_gyro' in obs_names:  # Same as angular velocity
+            obs_reps[obs_name] = rep_Rd_pseudo
+        else:
+            raise ValueError(f'Invalid observation name: {obs_name}')
+
+    return G, obs_reps
