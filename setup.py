465d9cbd03f04cf23505a5178feef019b9ad6b92
giulioturrisi <giulioturrisi@hotmail.it>
2025-04-30 09:27:58 +0200

    rename

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ setuptools.setup(
      name="sub-module",     
      version="0.0.1",
      python_requires=">=3.10",   
-     packages=["symmetry_aware_rsl_rl"],
+     packages=["symmetry-aware-rsl-rl"],
 )
